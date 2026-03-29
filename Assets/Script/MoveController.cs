ae445cbab114ec90cb985cd1259dcdab93a1cea8
weilungu <s1131375@o365st.pu.edu.tw>
2026-03-29 14:17:04 +0800

    add MoveController Script

---
+++ Assets/Script/MoveController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    float vertical;
+    float horizontal;
+    
+    [SerializeField] float speed;
+
+    Vector2 GetMoveInput()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        move = Vector2.ClampMagnitude(move, 1f);
+
+        return move;
+    }
+
+    void Movement()
+    {
+        Vector2 move = GetMoveInput();
+        if (move != Vector2.zero)
+        {
+            float dt = Time.deltaTime;
+
+            transform.Translate(move * speed * dt);
+        }
+    }
+}
