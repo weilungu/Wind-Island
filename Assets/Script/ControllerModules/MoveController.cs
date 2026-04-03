553e692d183f787872c9efeca16be6a509b6c413
weilungu <s1131375@o365st.pu.edu.tw>
2026-04-03 13:12:24 +0800

    same

--- Assets/Script/ControllerModules/MoveController.cs
+++ Assets/Script/ControllerModules/MoveController.cs
@@ -12,18 +12,22 @@ public class MoveController : MonoBehaviour
 
     public Vector2 GetMoveInput()
     {
-        // vertical = Input.GetAxisRaw("Vertical");
-        // horizontal = Input.GetAxisRaw("Horizontal");
-
-        // Vector2 move = new Vector2(horizontal, vertical);
-        // move = Vector2.ClampMagnitude(move, 1f);
-
-        return Vector2.zero;
+        int vertical=0, horizontal=0;
+        if (inp.moveUpPressed || inp.moveDownPressed)
+        {
+            vertical = inp.moveUpPressed ? 1 : -1;
+        }
+        else if (inp.moveLeftPressed || inp.moveRightPressed)
+        {
+            horizontal = inp.moveRightPressed ? 1 : -1;
+        }
+        
+        return new Vector2(horizontal, vertical);
     }
 
     public void Movement()
     {
-        Vector2 move = inp.GetMoveInput();
+        Vector2 move = GetMoveInput();
         if (move != Vector2.zero)
         {
             float dt = Time.deltaTime;
