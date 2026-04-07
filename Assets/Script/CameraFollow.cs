c7082f57dc35695899604e7a61972e4ceaea47c7
weilungu <s1131375@o365st.pu.edu.tw>
2026-04-07 11:40:02 +0800

    Camera Confiner

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -9,6 +9,10 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Vector3 offset = new Vector3(0, 0, -10);
     [SerializeField] float smoothTime = 0.3f;
     
+    [Header("Border")]
+    [SerializeField] Vector2 min;
+    [SerializeField] Vector2 max;
+    
     Vector3 velocity = Vector3.zero;
 
     void LateUpdate()
@@ -21,5 +25,10 @@ public class CameraFollow : MonoBehaviour
             ref velocity,
             smoothTime
         );
+        
+        float clampedX = Mathf.Clamp(transform.position.x, min.x, max.x);
+        float clampedY = Mathf.Clamp(transform.position.y, min.y, max.y);
+        
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
