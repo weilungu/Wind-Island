589a4211db13744131955218cc48e023ac852d3a
weilungu <s1131375@o365st.pu.edu.tw>
2026-04-17 15:32:10 +0800

    Idle direction

--- Assets/Script/Charactor/PlayerController.cs
+++ Assets/Script/Charactor/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     float vertical;
     Vector2 direction = Vector2.zero;
+    Vector2 nonZeroDir = Vector2.right;
 
     // Instance
     Animator anim;
@@ -39,6 +40,10 @@ public class PlayerController : MonoBehaviour
         fsm.SetGameState(GameState.Idle);
         anim.SetBool(AnimParams.Idle, true);
         anim.SetBool(AnimParams.Move, false);
+        anim.SetBool(AnimParams.IdleUp, false);
+        anim.SetBool(AnimParams.IdleDown, false);
+        anim.SetBool(AnimParams.MoveUp, false);
+        anim.SetBool(AnimParams.MoveDown, false);
     }
 
     void Update()
@@ -71,9 +76,20 @@ public class PlayerController : MonoBehaviour
         switch (fsm.gameState)
         {
             case GameState.Idle:
+                // Animation
                 anim.SetBool(AnimParams.Idle, true);
                 anim.SetBool(AnimParams.Move, false);
-                
+
+                if (nonZeroDir.y > 0.01f)
+                {
+                    anim.SetBool(AnimParams.IdleUp, true);
+                }
+                else if (nonZeroDir.y < -0.01f)
+                {
+                    anim.SetBool(AnimParams.IdleDown, true);
+                }
+
+                // Transition
                 if (inp.movePressed)
                 {
                     fsm.SetGameState(GameState.Move);
@@ -107,8 +123,10 @@ public class PlayerController : MonoBehaviour
         switch (fsm.gameState)
         {
             case GameState.Move:
-
+                // Direction
                 direction = new Vector2(horizontal, vertical).normalized;
+                if (!direction.Equals(Vector2.zero)) nonZeroDir = direction;
+                
                 attack.UpdateAttackDirection(direction);
                 move.Movement(direction);
                 
@@ -121,9 +139,29 @@ public class PlayerController : MonoBehaviour
                     sprite.flipX = false;
                 }
                 
+                // Animation
                 anim.SetBool(AnimParams.Move, true);
                 anim.SetBool(AnimParams.Idle, false);
+                anim.SetBool(AnimParams.IdleUp, false);
+                anim.SetBool(AnimParams.IdleDown, false);
+
+                if (direction.y > 0.01f) // Move Up
+                {
+                    anim.SetBool(AnimParams.MoveUp, true);
+                    anim.SetBool(AnimParams.MoveDown, false);
+                }
+                else if (direction.y < -0.01f) // Move Down
+                {
+                    anim.SetBool(AnimParams.MoveDown, true);
+                    anim.SetBool(AnimParams.MoveUp, false);
+                }
+                else
+                {
+                    anim.SetBool(AnimParams.MoveUp, false);
+                    anim.SetBool(AnimParams.MoveDown, false);
+                }
                 
+                // Transition
                 if (direction.Equals(Vector2.zero))
                 {
                     fsm.SetGameState(GameState.Idle);
--- Assets/Script/Others/AnimParams.cs
+++ Assets/Script/Others/AnimParams.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public static class AnimParams
 {
     public static readonly int Move = Animator.StringToHash("Move");
+    public static readonly int MoveUp = Animator.StringToHash("MoveUp");
+    public static readonly int MoveDown = Animator.StringToHash("MoveDown");
+    
     public static readonly int Idle = Animator.StringToHash("Idle");
+    public static readonly int IdleUp = Animator.StringToHash("IdleUp");
+    public static readonly int IdleDown = Animator.StringToHash("IdleDown");
+    
     public static readonly int Attack = Animator.StringToHash("Attack");
     
 }
