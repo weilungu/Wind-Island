1ac8d714f0285ee5b0efc92a4c8d49ea77d71b3e
weilungu <s1131375@o365st.pu.edu.tw>
2026-04-18 19:26:14 +0800

    Hidden cursor

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 }
