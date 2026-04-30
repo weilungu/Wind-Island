6edfdfd9a1e451fc8df2232ea77e2bf69185200c
weilungu <s1131375@o365st.pu.edu.tw>
2026-04-30 09:30:48 +0800

    remove something

--- Assets/Script/Scriptableobject/DashData.cs
+++ Assets/Script/Scriptableobject/DashData.cs
@@ -5,15 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DashData", menuName = "Dash/DashData")]
 public class DashData : ScriptableObject
 {
-    [Header("Be Dashed")]
-    public bool canDash = true;
-    public bool isDashing = false;
-    
     [Header("Dash Values")]
     public float dashSpeed = 12;
     public float dashDuration = 0.3f;
     public float dashCooldown = 0.5f;
-
-    // [Header("Posture")]
-    // public float posture = 10;
 }
