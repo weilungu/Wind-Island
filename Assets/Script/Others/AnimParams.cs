36b533f384b13801bc3cbe4a0e138a87232c5fb5
weilungu <s1131375@o365st.pu.edu.tw>
2026-05-05 20:59:17 +0800

    append Down trigger parameter

--- Assets/Script/Others/AnimParams.cs
+++ Assets/Script/Others/AnimParams.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public static class AnimParams
 {
-    
+
+    // Trigger
     public static readonly int Attack = Animator.StringToHash("Attack");
-    
+    public static readonly int Down = Animator.StringToHash("Down");
+
+
+    // Float
     public static readonly int MoveX = Animator.StringToHash("MoveX");
     public static readonly int MoveY = Animator.StringToHash("MoveY");
+
+
+    // Bool
     public static readonly int IsMoving = Animator.StringToHash("IsMoving");
 }
