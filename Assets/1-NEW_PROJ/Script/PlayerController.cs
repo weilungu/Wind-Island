07f9edfbc07b3354f4542f0e15abe11e1be0dc43
weilungu <s1131375@o365st.pu.edu.tw>
2026-05-30 18:13:55 +0800

    Player Dash

--- Assets/1-NEW_PROJ/Script/PlayerController.cs
+++ Assets/1-NEW_PROJ/Script/PlayerController.cs
@@ -6,16 +6,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     PlayerMovement move;
-
+    PlayerDash dash;
+    
     
     // == Unity API == //
     void Awake()
     {
         move = GetComponent<PlayerMovement>();
+        dash = GetComponent<PlayerDash>();
     }
 
     void FixedUpdate()
     {
+        if (dash.IsDashing) return;
+        
         move.Movement();
     }
 }
--- Assets/1-NEW_PROJ/Script/PlayerDash.cs
+++ Assets/1-NEW_PROJ/Script/PlayerDash.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Awake
+    Rigidbody2D rb;
+    
+    [Header("Inspector Values")]
+    [SerializeField] float dashSpeed = 1f;
+    [SerializeField] float dashDuration = 1f;
+    [SerializeField] float dashCooldown = 1f;
+    
+    // Private Values
+    [Header("Show")]
+    [SerializeField] bool canDash = true;
+    [SerializeField] bool isDashing = false;
+    
+    
+    // == Unity API == //
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
+    
+    // == Self API == //
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            StartCoroutine(DashRoutine());
+        }
     }
+
+    public IEnumerator DashRoutine()
+    {
+        Vector2 dashDir = PlayerMovement.Direction;
+        if (dashDir == Vector2.zero) yield break;
+        
+        print("Dash started");
+        canDash = false;
+        isDashing = true;
+
+        // Dashing
+        rb.velocity = dashDir * dashSpeed;
+        yield return new WaitForSeconds(dashDuration);
+        
+        // Reset State
+        rb.velocity = Vector2.zero;
+        isDashing = false;
+        
+        
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+        print("Dash finished");
+    }
+    
+    public bool IsDashing => isDashing;
+    public bool CanDash => canDash;
 }
