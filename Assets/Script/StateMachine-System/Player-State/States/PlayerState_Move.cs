c553639e3e49a141c47c0775b1463a6779c286e6
weilungu <s1131375@o365st.pu.edu.tw>
2026-08-07 14:29:01 +0800

    Can Be Rename XY Move Input Name

--- Assets/Script/StateMachine-System/Player-State/States/PlayerState_Move.cs
+++ Assets/Script/StateMachine-System/Player-State/States/PlayerState_Move.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data/StateMachine/Player_State/Move", fileName = "PlayerState_Move")]
 public class PlayerState_Move : PlayerState
 {
-    private static readonly int MoveX = Animator.StringToHash("MoveX");
-    private static readonly int MoveY = Animator.StringToHash("MoveY");
+    [Header("Input Name")]
+    [SerializeField] string XInputName = "MoveX";
+    [SerializeField] string YInputName = "MoveY";
+    
+    private static int MoveX;
+    private static int MoveY;
+
+    public override void Enter()
+    {
+        base.Enter();
+        
+        MoveX = Animator.StringToHash(XInputName);
+        MoveY = Animator.StringToHash(YInputName);
+    }
 
     public override void LogicalUpdate()
     {
