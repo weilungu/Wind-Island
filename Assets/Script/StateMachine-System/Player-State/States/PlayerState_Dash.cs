6b1f6dbbcb160cfd81d107a99d0bcece91fa72a7
weilungu <s1131375@o365st.pu.edu.tw>
2026-08-13 18:39:16 +0800

    set field with Inspector

--- Assets/Script/StateMachine-System/Player-State/States/PlayerState_Dash.cs
+++ Assets/Script/StateMachine-System/Player-State/States/PlayerState_Dash.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 [CreateAssetMenu(menuName = "Data/StateMachine/Player_State/Dash", fileName = "PlayerState_Dash")]
 public class PlayerState_Dash : PlayerState
 {
+    [Header("Dash Posture")]
+    [SerializeField] int postureAmount = 10;
+    
     public override void Enter()
     {
         base.Enter();
         
         ctx.Get<PlayerDash>().TryDash();
-        ctx.Get<PlayerPosture>().TakePosture(10);
+        ctx.Get<PlayerPosture>().TakePosture(postureAmount);
     }
 
     public override void LogicalUpdate()
