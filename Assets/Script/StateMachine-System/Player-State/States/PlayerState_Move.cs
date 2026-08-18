9b9ada987273098aeb41dd9b6ff647e1ed8c8e32
weilungu <s1131375@o365st.pu.edu.tw>
2026-08-18 23:07:35 +0800

    Indentation CreateAssetMenu with PlayerState_Move

--- Assets/Script/StateMachine-System/Player-State/States/PlayerState_Move.cs
+++ Assets/Script/StateMachine-System/Player-State/States/PlayerState_Move.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Data/StateMachine/Player_State/Move", fileName = "PlayerState_Move")]
+[CreateAssetMenu(
+    menuName = "Data/StateMachine/Player_State/Move",
+    fileName = "PlayerState_Move"
+)]
 public class PlayerState_Move : PlayerState
 {
     public override void LogicalUpdate()
