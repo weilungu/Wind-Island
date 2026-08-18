127f78cd7aa33c61033b74c93524a4f2b21f62d8
weilungu <s1131375@o365st.pu.edu.tw>
2026-08-19 00:43:27 +0800

    Create Attack States and Organiz State Datas

---
+++ Assets/Script/StateMachine-System/Player-State/States/Attack/PlayerState_Attack_GuardBreak.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(
+    menuName = "Data/StateMachine/Player_State/Attack/GuardBreak",
+    fileName = "PlayerState_Attack_GuardBreak")
+]
+public class PlayerState_Attack_GuardBreak : PlayerState
+{
+    
+}
---
+++ Assets/Script/StateMachine-System/Player-State/States/Attack/PlayerState_Attack_Normal.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(
+    menuName = "Data/StateMachine/Player_State/Attack/Normal",
+    fileName = "PlayerState_Attack_Normal")
+]
+public class PlayerState_Attack_Normal : PlayerState
+{
+    
+}
