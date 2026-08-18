9fb1042279dd7f92e7d9a1ee0020b16efe88e1c7
weilungu <s1131375@o365st.pu.edu.tw>
2026-08-19 01:08:56 +0800

    Set Animation Name

--- Assets/Script/Actions/PlayerCombat.cs
+++ Assets/Script/Actions/PlayerCombat.cs
@@ -5,34 +5,11 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // === Awake Values === //
-    Animator anim;
-    
-    
-    [Header("Values")]
-    [SerializeField] string animationName = "Attack";
-    
-    // === Private Values === //
-    int animHash;
-    
-    
-    // === Unity Life Cycle === //
-    void OnEnable()
-    {
-        animHash = Animator.StringToHash(animationName);
-    }
-
-    void Awake()
-    {
-        anim = GetComponentInChildren<Animator>();
-    }
     
     
     // === Self API === //
     public void Attack()
     {
-        anim.Play(animHash);
-        
         print("Attacked");
     }
 }
