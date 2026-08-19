a490c7f4651f617502981178a73614638c4fcfdb
weilungu <s1131375@o365st.pu.edu.tw>
2026-08-19 16:45:06 +0800

    definition some field nouns

--- Assets/Script/Actions/PlayerCombat.cs
+++ Assets/Script/Actions/PlayerCombat.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    [Header("Count")]
+    [SerializeField] int maxAttackSteps = 3;
+    [SerializeField, Range(0f, 1f)] float attackResetTime = 0.5f;
+    
+    [Header("Cooldown")]
+    [SerializeField, Range(0f, 1f)] float attackCooldown = 0.5f;
+    [SerializeField, Range(0f, 1f)] float comboCooldown = 0.5f;
+    
+    [Header("Debug Show")]
+    [SerializeField] int currAttackStep = 0;
     
     
     // === Self API === //
@@ -12,4 +22,6 @@ public class PlayerCombat : MonoBehaviour
     {
         print("Attacked");
     }
+
+    public bool CanContiAttack => currAttackStep < maxAttackSteps;
 }
